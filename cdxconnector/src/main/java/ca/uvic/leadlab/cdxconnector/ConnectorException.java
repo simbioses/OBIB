8cf1ba01ef5b15226ab047bd629d7bbe3756544f
Oscar Costa <oscarcosta@gmail.com>
2019-02-15 16:14:48 -0800

    Exposing WS client methods to be used as library

---
+++ cdxconnector/src/main/java/ca/uvic/leadlab/cdxconnector/ConnectorException.java
@@ -0,0 +1,8 @@
+package ca.uvic.leadlab.cdxconnector;
+
+public class ConnectorException extends Exception {
+
+    public ConnectorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- cdxconnector/src/main/java/ca/uvic/leadlab/cdxconnector/WSClient.java
+++ cdxconnector/src/main/java/ca/uvic/leadlab/cdxconnector/WSClient.java
@@ -1,12 +1,10 @@
 package ca.uvic.leadlab.cdxconnector;
 
-import ca.infoway.messagebuilder.model.ClinicalDocumentBean;
 import ca.interiorhealth.BizTalkServiceInstance;
 
-import ca.leadlab.obib.doctr.DocumentTransformer;
-import ca.leadlab.obib.doctr.ReferralRequestCreator;
 import ca.uvic.leadlab.cdxconnector.messages.ListClinicBuilder;
 import ca.uvic.leadlab.cdxconnector.messages.ListProviderBuilder;
+import ca.uvic.leadlab.cdxconnector.messages.MessageBuilderException;
 import ca.uvic.leadlab.cdxconnector.messages.SubmitDocumentBuilder;
 import org.hl7.v3.*;
 import org.xml.sax.SAXException;
@@ -27,11 +25,11 @@ import javax.xml.ws.handler.HandlerResolver;
 import javax.xml.ws.handler.PortInfo;
 import java.io.FileInputStream;
 import java.io.StringWriter;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.TimeZone;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -40,57 +38,42 @@ public class WSClient {
 
     private final Logger LOGGER = Logger.getLogger(WSClient.class.getName());
 
-    // Base URLs
-    //private String baseUrl = "https://servicesdev.bccdx.ca/";
-    //private String baseUrl = "https://servicestest.bccdx.ca/";
-    //private String baseUrl = "https://services.bccdx.ca/";
+    // Base URL
+    private String baseUrl;
 
-    // Clinic credentials
-    private String username = "cdxpostprod-otca";
-    private String password = "VMK31";
-    private String locationId = username;
-    private String clinicName = "Oscar Test Clinic A";
+    // Location credentials
+    private String username;
+    private String password;
+    private String locationId;
 
-    private String certPath = "/home/ocosta/Projects/UVic/LEADLab/source/OBIB/cdxconnector/certs/LEADlab_Keystore.jks";
-    private String certPass = "LEADlab";
+    // Certificate
+    private String certPath;
+    private char[] certPass;
 
     public static void main(String[] args) {
-        WSClient client = new WSClient();
+        try {
+            WSClient client = new WSClient("https://servicestest.bccdx.ca",
+                    "cdxpostprod-otca", "VMK31", "cdxpostprod-otca",
+                    "./certs/LEADlab_Keystore.jks", "LEADlab");
 
-        //client.listClinics();
-
-        //client.listProviders();
-
-        ClinicalDocumentBean referralRequest = new ReferralRequestCreator()
-                .loincCode("34117-2", "History &amp;Physical Note"  )
-                .patientName("Weber", "Jens")
-                .patientGender("MALE")
-                .patientDOB(2019,2,1)
-                .patientId("34234324")
-                .patientAddress("5470 Old West Saanich Rd.","Victoria","BC","V9E2A7","CA")
-                .patientTelecom("(250)721-8797")
-                .effectiveTime(2019,2,1, 12, 30, TimeZone.getTimeZone("GMT-8"))
-                .docId(UUID.randomUUID().toString())
-                .authorId("2343242")
-                .authorAddress("3800 Finnerty Rd", "Victoria", "BC","V8P 5C3", "CA")
-                .authorTelecom("(250) 472-8743")
-                .authorTime(2019,2,1)
-                .authorPersonName("Price","Morgan")
-                .custodianOrgazinationId("3234242")
-                .custodianOrgazinationName("Ocean Spree Medical")
-                .body("Please cure this person!")
-                .get();
-
-        DocumentTransformer documentTransformer = new DocumentTransformer();
-        String document = documentTransformer.parseDocument(referralRequest);
-
-        // TODO temporary solution to remove non-necessary tags from the XML
-        document = document.replaceAll("<realmCode\\s*code=\"((?!CA-BC).)*?\"/>|<templateId((?!assigningAuthorityName).)*?/>|<(inFulfillment|componentOf).*?/>", "");
-
-        client.submitDocument(document);
+            client.listClinics();
+        } catch (ConnectorException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void submitDocument(String document) {
+    public WSClient(String baseUrl, String username, String password, String locationId, String certPath, String certPass) throws ConnectorException {
+        this.baseUrl = baseUrl;
+        this.username = username;
+        this.password = password;
+        this.locationId = locationId;
+        this.certPath = certPath;
+        this.certPass = certPass.toCharArray();
+
+        setupSSLContext();
+    }
+
+    public String submitDocument(String document) throws ConnectorException {
         try {
             RCMRIN000002UV01 request = new SubmitDocumentBuilder(UUID.randomUUID().toString()) // Unique Message ID (GUID)
                     .receiver("CDX") // ID Of receiver
@@ -101,21 +84,21 @@ public class WSClient {
             System.out.println("\nSubmit Document Request:\n");
             System.out.println(parseObject(request));
 
-            setupSSLContext(certPath, certPass.toCharArray());
-
-            BizTalkServiceInstance documentService = new BizTalkServiceInstance(new URL("https://servicestest.bccdx.ca/CDASubmitService/CDASubmit.svc?WSDL"));
+            BizTalkServiceInstance documentService = new BizTalkServiceInstance(new URL(baseUrl + "/CDASubmitService/CDASubmit.svc?WSDL"));
             documentService.setHandlerResolver(handlerResolver(documentService.getServiceName()));
             MCCIIN000002UV01 response = documentService.getCustomBindingITwoWayAsync().submitCDA(request);
 
             System.out.println("\nSubmit Document Response:\n");
             System.out.println(parseObject(response));
 
-        } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "Error on submitDocument", e);
+            return parseObject(response);
+        } catch (MessageBuilderException | MalformedURLException e) {
+            LOGGER.log(Level.SEVERE, "Error submitting document", e);
+            throw new ConnectorException("Error submitting document", e);
         }
     }
 
-    private void listClinics() {
+    public String listClinics() throws ConnectorException {
         try {
             PRPMIN406010UV01 request = new ListClinicBuilder(UUID.randomUUID().toString()) // Unique Message ID (GUID)
                     .sender(locationId) // ID Of requestor
@@ -125,21 +108,21 @@ public class WSClient {
             System.out.println("\nList Clinics Request:\n");
             System.out.println(parseObject(request));
 
-            setupSSLContext(certPath, certPass.toCharArray());
-
-            ClinicQuery clinicQuery = new ClinicQuery(new URL("https://servicestest.bccdx.ca/RegistrySearch/ClinicQuery.svc?WSDL"));
+            ClinicQuery clinicQuery = new ClinicQuery(new URL(baseUrl + "/RegistrySearch/ClinicQuery.svc?WSDL"));
             clinicQuery.setHandlerResolver(handlerResolver(clinicQuery.getServiceName()));
             PRPMIN406110UV01 response = clinicQuery.getCustomBindingPRPMAR400013UV().prpmIN406010UV01(request);
 
             System.out.println("\nList Clinics Response:\n");
             System.out.println(parseObject(response));
 
-        } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "Error on listClinics", e);
+            return parseObject(response);
+        } catch (MalformedURLException e) {
+            LOGGER.log(Level.SEVERE, "Error listing clinics", e);
+            throw new ConnectorException("Error listing clinics", e);
         }
     }
 
-    private void listProviders() {
+    public String listProviders() throws ConnectorException {
         try {
             PRPMIN306010UV request = new ListProviderBuilder(UUID.randomUUID().toString())
                     .sender(locationId)
@@ -149,36 +132,37 @@ public class WSClient {
             System.out.println("\nList Provider Request:\n");
             System.out.println(parseObject(request));
 
-            setupSSLContext(certPath, certPass.toCharArray());
-
-            ProviderQuery providerQuery = new ProviderQuery(new URL("https://servicestest.bccdx.ca/RegistrySearch/ProviderQuery.svc?WSDL"));
+            ProviderQuery providerQuery = new ProviderQuery(new URL(baseUrl + "/RegistrySearch/ProviderQuery.svc?WSDL"));
             providerQuery.setHandlerResolver(handlerResolver(providerQuery.getServiceName()));
             PRPMIN306011UV response = providerQuery.getCustomBindingPRPMAR300013UV().prpmIN306010UV(request);
 
             System.out.println("\nList Provider Response:\n");
             System.out.println(parseObject(response));
 
-        } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "Error on listProviders", e);
+            return parseObject(response);
+        } catch (MalformedURLException e) {
+            LOGGER.log(Level.SEVERE, "Error listing providers", e);
+            throw new ConnectorException("Error listing providers", e);
         }
     }
 
-    private void setupSSLContext(String certPath, char[] certPassword) {
+    private void setupSSLContext() throws ConnectorException {
         try {
             SSLContext context = SSLContext.getInstance("SSLv3");
 
             KeyManagerFactory factory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
 
             KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            keyStore.load(new FileInputStream(certPath), certPassword);
+            keyStore.load(new FileInputStream(certPath), certPass);
 
-            factory.init(keyStore, certPassword);
+            factory.init(keyStore, certPass);
 
             context.init(factory.getKeyManagers(), null, null);
 
             HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
         } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "Error on setupSSLContext", e);
+            LOGGER.log(Level.SEVERE, "Error initializing ssl context", e);
+            throw new ConnectorException("Error initializing ssl context", e);
         }
     }
 
@@ -195,7 +179,7 @@ public class WSClient {
         };
     }
 
-    private String parseObject(Object obj) {
+    private String parseObject(Object obj) throws ConnectorException {
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(obj.getClass());
             Marshaller marshaller = jaxbContext.createMarshaller();
@@ -205,13 +189,12 @@ public class WSClient {
             marshaller.marshal(obj, writer);
 
             return writer.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (JAXBException e) {
+            throw new ConnectorException("Error parsing object", e);
         }
-        return null;
     }
 
-    String validateObject(Object obj, URL schemaUrl) {
+    private void validateObject(Object obj, URL schemaUrl) throws ConnectorException {
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(obj.getClass());
             SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
@@ -221,9 +204,7 @@ public class WSClient {
             marshaller.setSchema(schema);
             marshaller.marshal(obj, new DefaultHandler());
         } catch (JAXBException | SAXException e) {
-            LOGGER.log(Level.SEVERE, "Error validating object.", e);
-            return e.getMessage();
+            throw new ConnectorException("Error validating object", e);
         }
-        return null;
     }
 }

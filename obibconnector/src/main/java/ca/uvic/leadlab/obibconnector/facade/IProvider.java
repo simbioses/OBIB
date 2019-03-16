7d748d74bbfe9cac8ecd4a7551043783242e5440
Jens Weber <jens@uvic.ca>
2019-03-15 17:54:42 -0700

    added interfaces for registry queries

---
+++ obibconnector/src/main/java/ca/uvic/leadlab/obibconnector/facade/IProvider.java
@@ -0,0 +1,12 @@
+package ca.uvic.leadlab.obibconnector.facade;
+
+public interface IProvider {
+
+    String getFirstName();
+    String getLastName();
+    String getPrefix();
+    String getID();
+    String getClinicID();
+    String getClinicName();
+
+}

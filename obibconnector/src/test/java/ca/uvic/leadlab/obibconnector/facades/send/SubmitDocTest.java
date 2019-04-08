7fbdd74587651eebf81a68acb27ccf35e9a34c5a
Oscar Costa <oscarcosta@gmail.com>
2019-04-08 10:11:17 -0700

    Adjust document on submit test.

--- obibconnector/src/test/java/ca/uvic/leadlab/obibconnector/facades/send/SubmitDocTest.java
+++ obibconnector/src/test/java/ca/uvic/leadlab/obibconnector/facades/send/SubmitDocTest.java
@@ -14,7 +14,8 @@ public class SubmitDocTest extends FacadesBaseTest {
     public void testSubmitDoc() throws Exception {
         ISubmitDoc submitDoc = new SubmitDoc(config);
 
-        String response = submitDoc.patient()
+        String response = submitDoc.newDoc()
+                        .patient()
                             .id("2222")
                             .name(NameType.LEGAL, "Joe", "Wine")
                             .address(AddressType.HOME, "111 Main St", "Victoria", "BC", "V8V Z9Z", "CA")
@@ -31,6 +32,7 @@ public class SubmitDocTest extends FacadesBaseTest {
                             .address(AddressType.HOME, "111 Main St", "Victoria", "BC", "V8V Z9Z", "CA")
                             .phone(TelcoType.HOME, "250-111-1234")
                         .and().participant()
+                            .functionCode("PCP")
                             .id("555")
                             .name(NameType.LEGAL, "Joseph", "Cloud")
                             .address(AddressType.HOME, "111 Main St", "Victoria", "BC", "V8V Z9Z", "CA")

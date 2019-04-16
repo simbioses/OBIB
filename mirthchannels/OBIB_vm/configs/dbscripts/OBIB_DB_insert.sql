b9080e2f9c2bbe74f8e135d2e67c9e68129a9afd
Oscar Costa <oscarcosta@gmail.com>
2019-04-16 13:58:19 -0700

    Updating VM with certificates in pkcs12 format

--- mirthchannels/OBIB_vm/configs/dbscripts/OBIB_DB_insert.sql
+++ mirthchannels/OBIB_vm/configs/dbscripts/OBIB_DB_insert.sql
@@ -17,6 +17,6 @@ INSERT INTO OBIB_DB.cdx_id (id, root, extension, name) VALUES (16, null, '2.16.8
 
 
 INSERT INTO OBIB_DB.clinic_credential (clinic_id, clinic_name, clinic_username, clinic_password, certificate_file, certificate_password) 
-  VALUES ('cdxpostprod-obctc', 'Oscar Test Clinic C', 'cdxpostprod-obctc', '2LsTebRe', 'LEADlab_Keystore.jks', 'LEADlab');
+  VALUES ('cdxpostprod-obctc', 'Oscar Test Clinic C', 'cdxpostprod-obctc', '2LsTebRe', 'OscarTestClinic.pfx', 'LEADlab');
 INSERT INTO OBIB_DB.clinic_credential (clinic_id, clinic_name, clinic_username, clinic_password, certificate_file, certificate_password) 
-  VALUES ('cdxpostprod-otca', 'Oscar Test Clinic A', 'cdxpostprod-otca', 'VMK31', 'LEADlab_Keystore.jks', 'LEADlab');
+  VALUES ('cdxpostprod-otca', 'Oscar Test Clinic A', 'cdxpostprod-otca', 'VMK31', 'LEADlab_IHA_cert.pfx', 'LEADlab');

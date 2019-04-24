b05b35c798d54055009ce567f6b7b7f16bd5242b
Oscar Costa <oscarcosta@gmail.com>
2019-04-24 12:19:49 -0700

    Returning the first ID if default is not available; Refactor: extract method to return firstName;

--- obibconnector/src/main/java/ca/uvic/leadlab/obibconnector/impl/registry/Provider.java
+++ obibconnector/src/main/java/ca/uvic/leadlab/obibconnector/impl/registry/Provider.java
@@ -39,11 +39,7 @@ public class Provider implements IProvider {
         ID = ImplHelper.getDefaultProviderId(provider.getIds());
 
         if (provider.getName() != null) {
-            firstName = "";
-            for (String given : provider.getName().getGiven()) {
-                firstName = String.format("%s %s", firstName, given); // Concatenates the first name
-            }
-            firstName = firstName.trim();
+            firstName = ImplHelper.getFirstName(provider.getName().getGiven());
             lastName = provider.getName().getFamily();
             prefix = provider.getName().getPrefix();
         }

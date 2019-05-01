5b9b328a8677d3a15f6c12609e7b51445b402203
Oscar Costa <oscarcosta@gmail.com>
2019-04-30 17:27:58 -0700

    Improved cdxconnector tests creation

--- cdxconnector/src/test/java/ca/uvic/leadlab/cdxconnector/TestWSClientDocument.java
+++ cdxconnector/src/test/java/ca/uvic/leadlab/cdxconnector/TestWSClientDocument.java
@@ -2,16 +2,26 @@ package ca.uvic.leadlab.cdxconnector;
 
 import ca.uvic.leadlab.cdxconnector.messages.DocumentQueryParameterBuilder;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestWSClientDocument extends TestWSClient {
 
+    static WSClientDocument wsClientDocumentA;
+    static WSClientDocument wsClientDocumentC;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        wsClientDocumentA = new WSClientDocument(cdxServerUrl, ClinicA.username, ClinicA.password,
+            ClassLoader.getSystemClassLoader().getResource(ClinicA.certificate).getFile(), ClinicA.certPassword);
+
+        wsClientDocumentC = new WSClientDocument(cdxServerUrl, ClinicC.username, ClinicC.password,
+                ClassLoader.getSystemClassLoader().getResource(ClinicC.certificate).getFile(), ClinicC.certPassword);
+    }
+
     @Test
     public void testSubmitDocument() throws Exception {
-        WSClientDocument document = new WSClientDocument(cdxServerUrl, oscarClinic1Username, oscarClinic1Password,
-                getClass().getClassLoader().getResource(oscarClinic1Certificate).getFile(), oscarClinic1certPassword);
-
-        String response = document.submitDocument("cdxpostprod-otca", "", null, "cdxpostprod-octca");
+        String response = wsClientDocumentA.submitDocument(ClinicA.id, "", null, ClinicC.id);
 
         Assert.assertNotNull(response);
         System.out.println(TestUtils.prettyXML(response));
@@ -19,10 +29,7 @@ public class TestWSClientDocument extends TestWSClient {
 
     @Test
     public void testListNewDocuments() throws Exception {
-        WSClientDocument document = new WSClientDocument(cdxServerUrl, oscarClinic1Username, oscarClinic1Password,
-                getClass().getClassLoader().getResource(oscarClinic1Certificate).getFile(), oscarClinic1certPassword);
-
-        String response = document.listNewDocuments("cdxpostprod-otca");
+        String response = wsClientDocumentA.listNewDocuments(ClinicA.id);
 
         Assert.assertNotNull(response);
         System.out.println(TestUtils.prettyXML(response));
@@ -30,10 +37,7 @@ public class TestWSClientDocument extends TestWSClient {
 
     @Test
     public void testGetDocument() throws Exception {
-        WSClientDocument document = new WSClientDocument(cdxServerUrl, oscarClinic1Username, oscarClinic1Password,
-                getClass().getClassLoader().getResource(oscarClinic1Certificate).getFile(), oscarClinic1certPassword);
-
-        String response = document.getDocument("cdxpostprod-otca", "8a96c1d7-2823-e911-a96a-0050568c55a6");
+        String response = wsClientDocumentA.getDocument(ClinicA.id, "8a96c1d7-2823-e911-a96a-0050568c55a6");
 
         Assert.assertNotNull(response);
         System.out.println(TestUtils.prettyXML(response));
@@ -41,12 +45,11 @@ public class TestWSClientDocument extends TestWSClient {
 
     @Test
     public void testSearchDocuments() throws Exception {
-        WSClientDocument document = new WSClientDocument(cdxServerUrl, oscarClinic1Username, oscarClinic1Password,
-                getClass().getClassLoader().getResource(oscarClinic1Certificate).getFile(), oscarClinic1certPassword);
-
-        String response = document.searchDocuments("cdxpostprod-otca", new DocumentQueryParameterBuilder()
-                .clinicId("cdxpostprod-otca"));
-
+        String response = wsClientDocumentC.searchDocuments(ClinicA.id, new DocumentQueryParameterBuilder()
+                //.clinicId(ClinicA.id));
+        //.documentId("801068d3-0c67-45a4-ba99-2f767e559733"));
+        //.documentId("9965cc95-29df-4a4e-be26-93269d7a46c4"));
+        );
         Assert.assertNotNull(response);
         System.out.println(TestUtils.prettyXML(response));
     }

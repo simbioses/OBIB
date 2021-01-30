1b150773317767b9671b9c2ee1fd6de576f30cb0
Oscar Costa <oscarcosta@gmail.com>
2021-01-29 21:38:15 -0800

    Including loinc codes

--- mirthchannels/OBIB_vm/configs/dbscripts/OBIB_DB_insert_loinc.sql
+++ mirthchannels/OBIB_vm/configs/dbscripts/OBIB_DB_insert_loinc.sql
@@ -6,7 +6,9 @@ INSERT INTO cdx_loinc_code (loinc_code, loinc_name, template_id, template_name) 
 INSERT INTO cdx_loinc_code (loinc_code, loinc_name, template_id, template_name) VALUES ('11488-4', 'Consult Note', '2.16.840.1.113883.10.20.4', 'Consultation Note');
 INSERT INTO cdx_loinc_code (loinc_code, loinc_name, template_id, template_name) VALUES ('18842-5', 'Discharge Summary', '2.16.840.1.113883.3.51.60.2.4','Discharge Summary');
 INSERT INTO cdx_loinc_code (loinc_code, loinc_name, template_id, template_name) VALUES ('81200-8', 'Interdisciplinary Plan of Care', '', '');
-INSERT INTO cdx_loinc_code (loinc_code, loinc_name, template_id, template_name) VALUES ('X10916', 'Information Request', '', '');
+INSERT INTO cdx_loinc_code (loinc_code, loinc_name, template_id, template_name) VALUES ('X10916', 'Information Request', '2.16.840.1.113883.10.20.19', 'Unstructured Report');
 INSERT INTO cdx_loinc_code (loinc_code, loinc_name, template_id, template_name) VALUES ('34109-9', 'Note', '', '');
 INSERT INTO cdx_loinc_code (loinc_code, loinc_name, template_id, template_name) VALUES ('X10915', 'General-Purpose Notification', '', '');
+INSERT INTO cdx_loinc_code (loinc_code, loinc_name, template_id, template_name) VALUES ('60591-5', 'Patient Summary', '2.16.840.1.113883.10.20.19', 'Unstructured Report');
+INSERT INTO cdx_loinc_code (loinc_code, loinc_name, template_id, template_name) VALUES ('X10914', 'Advice Request', '2.16.840.1.113883.10.20.19', 'Unstructured Report');
 

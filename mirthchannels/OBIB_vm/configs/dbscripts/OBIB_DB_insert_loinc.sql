47a335b6fa2f1617f68efc12abc0e32c760df8aa
Oscar Costa <oscarcosta@gmail.com>
2021-05-17 13:24:30 -0700

    Fixing template id for information request loinc

--- mirthchannels/OBIB_vm/configs/dbscripts/OBIB_DB_insert_loinc.sql
+++ mirthchannels/OBIB_vm/configs/dbscripts/OBIB_DB_insert_loinc.sql
@@ -9,9 +9,9 @@ INSERT INTO cdx_loinc_code (id, loinc_code, loinc_name, template_id, template_na
 INSERT INTO cdx_loinc_code (id, loinc_code, loinc_name, template_id, template_name) VALUES (3, '11488-4', 'Consult Note', '2.16.840.1.113883.3.1818.10.1.4', 'e2e Unstructured Document');
 INSERT INTO cdx_loinc_code (id, loinc_code, loinc_name, template_id, template_name) VALUES (4, '18842-5', 'Discharge Summary', '2.16.840.1.113883.3.1818.10.1.4', 'e2e Unstructured Document');
 INSERT INTO cdx_loinc_code (id, loinc_code, loinc_name, template_id, template_name) VALUES (5, '81200-8', 'Interdisciplinary Plan of Care', '2.16.840.1.113883.3.1818.10.1.4', 'e2e Unstructured Document');
-INSERT INTO cdx_loinc_code (id, loinc_code, loinc_name, template_id, template_name) VALUES (6, 'X10916', 'Information Request',  '2.16.840.1.113883.3.1818.10.1.5', 'e2e Referral (Unstructured)');
+INSERT INTO cdx_loinc_code (id, loinc_code, loinc_name, template_id, template_name) VALUES (6, 'X10916', 'Information Request',  '2.16.840.1.113883.3.1818.10.1.4', 'e2e Unstructured Document');
 INSERT INTO cdx_loinc_code (id, loinc_code, loinc_name, template_id, template_name) VALUES (7, '34109-9', 'Note', '2.16.840.1.113883.3.1818.10.1.4', 'e2e Unstructured Document');
-INSERT INTO cdx_loinc_code (id, loinc_code, loinc_name, template_id, template_name) VALUES (8, 'X10915', 'General-Purpose Notification', '2.16.840.1.113883.3.1818.10.1.4', 'e2e Unstructured Document');
+INSERT INTO cdx_loinc_code (id, loinc_code, loinc_name, template_id, template_name) VALUES (8, 'X10915', 'General Purpose Notification', '2.16.840.1.113883.3.1818.10.1.4', 'e2e Unstructured Document');
 INSERT INTO cdx_loinc_code (id, loinc_code, loinc_name, template_id, template_name) VALUES (9, '60591-5', 'Patient Summary', '2.16.840.1.113883.3.1818.10.1.4', 'e2e Unstructured Document');
 INSERT INTO cdx_loinc_code (id, loinc_code, loinc_name, template_id, template_name) VALUES (10, 'X10914', 'Advice Request', '2.16.840.1.113883.3.1818.10.1.5', 'e2e Referral (Unstructured)');
 
